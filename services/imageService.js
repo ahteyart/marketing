38b6da15fd4f0ed3e84e32d0d7ef64064e94bdc3
ahteyart <ahteyart@gmail.com>
2026-05-05 19:11:54 +0800

    Update model endpoint in imageService.js

--- services/imageService.js
+++ services/imageService.js
@@ -3,7 +3,7 @@ export async function generateImage(prompt) {
     "text, words, letters, typography, watermark, writing, captions, titles, labels, signs, fonts, alphabet, numbers, logo, ugly, deformed, blurry, low quality, bad anatomy";
 
   const response = await fetch(
-    "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+    "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev ",
     {
       method: "POST",
       headers: {
